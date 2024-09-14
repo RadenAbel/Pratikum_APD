fef89ec031a9a0aefdc6c8cccd3b4e724e1a467f
RadenAbel <92682448+RadenAbel@users.noreply.github.com>
2024-09-14 18:54:37 +0700

    Add files via upload

---
+++ posttest2.py
@@ -0,0 +1,10 @@
+nama_lengkap    = str(input("Input nama lengkap anda: "))
+nama_panggilan  = str(input("Input nama panggilan anda: "))
+nim             = int(input("Input NIM anda: "))
+prodi           = str(input("Input asal prodi anda: "))
+umur            = int(input("Input umur anda: "))
+berat_badan     = float(input("Input berat badan anda: "))
+mod1            = int(input("Input dua digit angka dibelakang nim anda: "))
+mod2            = 6
+
+print(f"Nama lengkap saya adalah {nama_lengkap} dengan nim {nim} prodi {prodi}, saya biasa dipanggil {nama_panggilan} sekarang saya berumur {umur} tahun dan berat badan saya adalah {berat_badan} -> {mod1/mod2}")
